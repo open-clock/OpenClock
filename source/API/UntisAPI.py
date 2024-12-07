4283205d805b8f8c97eaf8d6cd1630faee3344f3
Jan Weichhart <jani.weihhart@gmail.com>
2024-12-07 11:06:11 +0100

    fix: fixing up my code

--- source/API/UntisAPI.py
+++ source/API/UntisAPI.py
@@ -12,65 +12,39 @@ class credentials(BaseModel):
     server : str
     school : str
     useragent : str
-
-    def toJSON():
-        pass
-
 class cacheClass:
-    creds
-    session
+    creds : credentials
+    session : webuntis.session
     timeTable: list
-
+    holidays : list
 
 global cache
 cache = cacheClass()
-{
- "username": "40146720210116",
-  "password": "187",
-  "server": "arche.webuntis.com",
-  "school": "litec",
-  "useragent": "WebUntis"
-}
-async def login():
-    """
-    #### Logging into a WebUntis session with the given credentials 
 
-    >>> login('40146720210116', '187', 'arche.webuntis.com', 'litec', 'WebUntis Test')
-    YourWebUntisSession
-    """
+
+async def setSession()->bool:
     try:
         global session
         session = webuntis.Session( 
-            username=creds.username,
-            password=creds.password,
-            server=creds.server,
-            school=creds.school,
-            useragent=creds.useragent
+            username=cache.creds.username,
+            password=cache.creds.password,
+            server=cache.creds.server,
+            school=cache.creds.school,
+            useragent=cache.creds.useragent
         ).login()
-        return session
-    except webuntis.errors.BadCredentialsError or webuntis.errors.NotLoggedInError:
-        return "Not valide credentials"
-
-@app.get("/canloginAs-Untis/{username}/{password}/{server}/{school}/{useragent}s")
-def canLoginAs(username: str, password: str, server: str, school: str, useragent: str)->bool:
-    """
-    #### Trys to login into a WebUnti session with the given credentials
-    #### returns False if the credentials are not valide 
-
-    >>> canlogin('40146720210116', '187', 'arche.webuntis.com', 'litec', 'WebUntis Test')
-    True
-    """
-    try:   
-        webuntis.Session( 
-            username=username,
-            password=password,
-            server=server,
-            school=school,
-            useragent=useragent
-        ).login().logout()
+        return True
     except webuntis.errors.BadCredentialsError or webuntis.errors.NotLoggedInError:
         return False
+async def setTimeTable(dayRange: int = 1)->bool:
+    global cache
+    now: datetime.date = datetime.datetime.now()
+    cache.timeTable = sorted(cache.session.my_timetable(start= now,end=now+datetime.timedelta(days=dayRange)), key=lambda x: x.start, reverse=False)
     return True
+async def setNextHoliday()->bool:
+    global cache
+    cache.holidays = cache.session.holidays()
+    return True
+async def setNextEvent(maxDeph = 14)->bool:
 
 
 @app.post("/set-cred")
@@ -80,24 +54,24 @@ async def setCreds(cred: credentials):
     return creds
 
 @app.get("/get-Timtable")
-async def getTimeTable(dayRange: int = 1):
-    s = await login()
-    now: datetime.date = datetime.datetime.now()
-    sortedTimeTable : list = sorted(s.my_timetable(start= now,end=now+datetime.timedelta(days=dayRange)), key=lambda x: x.start, reverse=False)
-    i = 0
-    retStr : str =""
-    for t in sortedTimeTable:
-        if i>1:
-            if t.start.day - sortedTimeTable[i-1].start.day >= 1:
-                retStr = retStr.__add__("---\n")
-        retStr = retStr.__add__(t.studentGroup)
-        retStr = retStr.__add__("\n")
-        i += 1;
-    return retStr
+async def getTimeTable(dayRange: int = 1)-> None:
+    global cache
+    return cache.timeTable
+    
 
-@app.get("/session")
-def getSession():
-    global session
-    if session is not None:
+def hasSession()->bool:
+    global cache
+    if cache.session is not None:
         return True
     return False
+
+
+
+@app.get("/update")
+async def update()->None:
+    global cache
+    if not hasSession():
+        setSession()
+    setTimeTable()
+    
+ 
